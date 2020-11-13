6114f171c01486d69d4540b87372fa96a5e48e5a
Seongchuel <aciddust20@gmail.com>
2020-11-13 17:54:45 +0900

    deprecated

--- crawler.py
+++ crawler.py
@@ -13,9 +13,6 @@ from fake_useragent import UserAgent # pip install fake-useragent
 from bs4 import BeautifulSoup # pip install beautifulsoup4
 from pprint import pprint
 from lxml.html import fromstring # pip install lxml
-from selenium import webdriver # pip install selenium
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from pyvirtualdisplay import Display
 from multiprocessing import Pool
 
