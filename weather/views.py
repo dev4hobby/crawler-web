dd3d1f5881019380811fae67d07114d21114e9f1
Seongchuel <aciddust20@gmail.com>
2020-11-14 14:08:46 +0900

    앱 이름을 Weather로 변경, 크롤링된 데이터는 DB에 저장

--- weather/views.py
+++ weather/views.py
@@ -9,7 +9,7 @@ from math import ceil
 
 class HomeView(TemplateView):
 
-    template_name = 'finder.html'
+    template_name = 'get-weather.html'
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         return context
